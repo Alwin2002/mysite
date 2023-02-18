b4dc1edef1f17cd897aa67987488c2ec53141b89
alwin <alwinantu0909@gmail.com>
2023-02-18 20:58:08 +0530

    building a  blog site

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.views.generic import ListView,TemplateView
-from .models import Post
+from django.views.generic import *
+from .models import *
 
 def index(request):
     return HttpResponse("jango")
@@ -12,7 +12,7 @@ def inde1(request):
     return HttpResponse('Fan')
 
 class ModelView(ListView):
-    model = Post
+    model = blog
     template_name = "home.html"
     context_object_name='post_list'
 
